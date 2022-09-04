f795c43f4ed5ce29aaad575575d5d77cdbaae883
Sarvess Veeriyah <sarvess.jr@outlook.com>
2022-09-04 22:32:48 +0800

    main

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
+//Setup Redux
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator>
+          <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+            headerShown: false
+          }}
+           />
+           <Stack.Screen
+            name="MapScreen"
+            component={MapScreen}
+            options={{
+              headerShown: false,
+            }}
+            />
+        </Stack.Navigator>
+   {/* <HomeScreen/> */}
+   </SafeAreaProvider>
+    </NavigationContainer>
+    </Provider>
   );
 }
 
